22dc7918993b5df2552d4bcac3297c6c3211d376
Defite <me@defite.ru>
2018-01-02 15:42:10 +0300

    Add NetlifyCMS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,30 @@
-import React from "react"
+import React, { Component } from "react"
+import Script from 'react-load-script'
 
-export default () => (
-  <div>
-    <h1>Index page</h1>
-    <p>Home page for everything</p>
-  </div>
-)
+export default class IndexPage extends Component {
+  handleScriptLoad() {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on('init', user => {
+        if (!user) {
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/';
+          });
+        }
+      });
+    }
+    window.netlifyIdentity.init();
+  }
+
+  render() {
+    return (
+      <div>
+        <Script
+          url="https://identity.netlify.com/v1/netlify-identity-widget.js"
+          onLoad={this.handleScriptLoad.bind(this)}
+        />
+        <h1>Index page</h1>
+        <p>Home page for everything</p>
+      </div>
+    )
+  }
+}
