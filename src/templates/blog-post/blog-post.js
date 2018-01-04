916c42ef359963fb763f186986eb480fbf1ffaff
Defite <me@defite.ru>
2018-01-04 23:43:46 +0300

    Added support for full-width images

--- src/templates/blog-post/blog-post.js
+++ src/templates/blog-post/blog-post.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './blog-post.module.css'
 
 export default ({ data }) => {
   const post = data.markdownRemark;
   return (
-    <div>
+    <div className={ styles.post }>
       <h1>{post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
     </div>
