e1aa422f5007914572e7abcfa668a01f84715fb3
Defite <me@defite.ru>
2019-12-07 22:13:01 +0300

    Move to separate module theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,150 +1,29 @@
-const postcssPresetEnv = require('postcss-preset-env');
-const fetch = require('node-fetch');
-const { createHttpLink } = require('apollo-link-http');
+const menu = require('./src/@defite/gatsby-theme-defite/langs/menu');
+const langs = Object.keys(menu);
 
 module.exports = {
-	siteMetadata: {
-		title: 'Nikita Makhov',
-		author: 'Nikita Makhov',
-		description: 'Frontend developer blog',
-		siteUrl: 'https://defite.ru',
-	},
-	plugins: [
-		{
-			resolve: 'gatsby-source-filesystem',
-			options: {
-				path: `${__dirname}/uploads`,
-				name: 'uploads',
-			},
-		},
-		{
-			resolve: 'gatsby-source-filesystem',
-			options: {
-				path: `${__dirname}/content/pages`,
-				name: 'pages',
-			},
-		},
-		{
-			resolve: 'gatsby-source-filesystem',
-			options: {
-				path: `${__dirname}/content/posts`,
-				name: 'posts',
-			},
-		},
-		{
-			resolve: 'gatsby-plugin-react-svg',
-			options: {
-				rule: {
-					include: /\.inline\.svg$/,
-				},
-			},
-		},
-		{
-			resolve: 'gatsby-transformer-remark',
-			options: {
-				plugins: [
-					{
-						resolve: 'gatsby-remark-relative-images',
-					},
-					'gatsby-remark-unwrap-images',
-					{
-						resolve: 'gatsby-remark-images',
-						options: {
-							maxWidth: 2560,
-						},
-					},
-					{
-						resolve: 'gatsby-remark-responsive-iframe',
-						options: {
-							wrapperStyle: 'margin-bottom: 1.0725rem',
-						},
-					},
-					'gatsby-remark-prismjs',
-					'gatsby-remark-copy-linked-files',
-					'gatsby-remark-smartypants',
-				],
-			},
-		},
-		'gatsby-transformer-sharp',
-		'gatsby-plugin-sharp',
-		'gatsby-plugin-feed',
-		{
-			resolve: 'gatsby-plugin-manifest',
-			options: {
-				name: 'Nikita Makhov',
-				short_name: 'Defite.ru',
-				start_url: '/',
-				background_color: '#ffffff',
-				theme_color: '#663399',
-				display: 'minimal-ui',
-				icon: 'src/assets/gatsby-icon.png',
-			},
-		},
-		'gatsby-plugin-offline',
-		'gatsby-plugin-react-helmet',
-		{
-			resolve: 'gatsby-plugin-typography',
-			options: {
-				pathToConfigModule: 'src/utils/typography',
-			},
-		},
-		{
-			resolve: 'gatsby-plugin-prefetch-google-fonts',
-			options: {
-				fonts: [
-					{
-						family: 'Fira Sans',
-						subsets: ['cyrillic'],
-						variants: ['400', '500', '600', '700', '800'],
-					},
-					{
-						family: 'Merriweather',
-						subsets: ['cyrillic'],
-						variants: ['400', '700'],
-					},
-				],
-			},
-		},
-		{
-			resolve: 'gatsby-plugin-postcss',
-			options: {
-				postCssPlugins: [
-					postcssPresetEnv({
-						stage: 3,
-						features: {
-							'nesting-rules': true,
-						},
-					}),
-				],
-			},
-		},
-		{
-			resolve: 'gatsby-source-graphql',
-			options: {
-				fieldName: 'github',
-				typeName: 'GitHub',
-				createLink: () => createHttpLink({
-					uri: 'https://api.github.com/graphql',
-					headers: {
-						Authorization: `bearer ${process.env.APP_GITHUB_TOKEN}`,
-					},
-					fetch,
-				}),
-			},
-		},
-		{
-			resolve: 'gatsby-plugin-i18n',
-			options: {
-				langKeyDefault: 'ru',
-				useLangKeyLayout: false,
-				pagesPaths: ['/pages/', '/posts/'],
-			},
-		},
-		{
-			resolve: 'gatsby-plugin-google-analytics',
-			options: {
-				trackingId: 'UA-143754409-1',
-			},
-		},
-	],
-};
+    plugins: [
+        {
+            resolve: '@defite/gatsby-theme-defite',
+            options: {
+                contentPath: '/content/',
+                siteMetadata: {
+                    title: 'Nikita Makhov',
+                    author: 'Nikita Makhov',
+                    description: 'Frontend developer blog',
+                    siteUrl: 'https://defite.ru',
+                },
+                manifestOptions: {
+                    name: 'Nikita Makhov',
+                    short_name: 'Defite.ru',
+                    start_url: '/',
+                    background_color: '#ffffff',
+                    theme_color: '#663399',
+                    display: 'minimal-ui',
+                    icon: 'assets/gatsby-icon.png'
+                },
+                langs
+            }
+        }
+    ]
+}
--- src/@defite/gatsby-theme-defite/langs/menu.js
+++ src/@defite/gatsby-theme-defite/langs/menu.js
@@ -1,24 +1,20 @@
-export default {
+module.exports = {
 	ru: {
 		title: 'Никита Махов',
 		menu: [
 			{
-				id: 1,
 				text: 'Главная',
 				link: '/',
 			},
 			{
-				id: 2,
 				text: 'Блог',
 				link: 'blog',
 			},
 			{
-				id: 3,
 				text: 'Обо мне',
 				link: 'about',
 			},
 			{
-				id: 4,
 				text: 'Резюме',
 				link: 'cv',
 			},
@@ -28,17 +24,14 @@ export default {
 		title: 'Nikita Makhov',
 		menu: [
 			{
-				id: 4,
 				text: 'Home',
 				link: '/',
 			},
 			{
-				id: 5,
 				text: 'Blog',
 				link: 'blog',
 			},
 			{
-				id: 6,
 				text: 'About',
 				link: 'about',
 			},
